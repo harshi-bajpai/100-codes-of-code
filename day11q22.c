12c14b44c8f932bc9ee1144f2bd55793694e4e7d
harshi-bajpai <balajipandit3115@gmail.com>
2026-08-29 12:51:41 +0530

    Add files via upload

---
+++ day11q22.c
@@ -0,0 +1,42 @@
+//Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 1200
+Output 1:
+Profit 20%
+
+Input 2:
+1000 800
+Output 2:
+Loss 20%
+
+Input 3:
+1000 1000
+Output 3:
+No Profit No Loss
+
+*/
+#include <stdio.h>
+
+int main() {
+    float costPrice, sellingPrice, percentage;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &costPrice);
+    printf("Enter Selling Price: ");
+    scanf("%f", &sellingPrice);
+
+    if (sellingPrice > costPrice) {
+        percentage = ((sellingPrice - costPrice) / costPrice) * 100;
+        printf("Profit Percentage: %.2f%%\n", percentage);
+    } else if (costPrice > sellingPrice) {
+        percentage = ((costPrice - sellingPrice) / costPrice) * 100;
+        printf("Loss Percentage: %.2f%%\n", percentage);
+    } else {
+        printf("No Profit, No Loss.\n");
+    }
+
+    return 0;
+}
