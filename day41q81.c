8d905ca338306f57b5c29dc9f0d39bd17dbc509e
harshi-bajpai <balajipandit3115@gmail.com>
2026-09-21 23:15:08 +0530

    Add files via upload

---
+++ day41q81.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[i] != '\0' && str[i] != '\n')
+    {
+        i++;
+    }
+
+    printf("Number of characters = %d", i);
+
+    return 0;
+}
